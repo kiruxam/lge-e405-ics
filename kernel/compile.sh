cbdd8e3ae466ce5fa682ac029215939e7976d4cf
kiruxam <neozone@ukr.net>
2015-04-08 17:13:19 +0300

    log

--- kernel/compile.sh
+++ kernel/compile.sh
@@ -3,5 +3,5 @@
   export CROSS_COMPILE=arm-none-eabi-
 #  make clean
   make e1brcis-perf_rev11_defconfig
-  make > ../01-log-01.txt
+  make > ../`date '+%Y-%m-%d'`-log-`date '+%H-%M'`.txt
 #  make menuconfig
